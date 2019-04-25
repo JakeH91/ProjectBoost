dfb33a6bdb55ca77c7ea2fff016b476b54721675
Jake Hill <jakehill@Jakes-MacBook-Pro.local>
2019-04-25 22:31:23 +0200

    create automatic oscillation of obstacles on level 3 (and reorder levels as this one is by far the hardest)

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -7,18 +7,23 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     [SerializeField] Vector3 movementVector;
-    [Range(0, 1)] [SerializeField] float movementFactor;
-    Vector3 startingPosition;
+    [SerializeField] float period = 3f;
 
-    // Start is called before the first frame update
+    Vector3 startingPosition;
+    float movementFactor;
+
     void Start()
     {
         startingPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = rawSinWave / 2f + 0.5f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
